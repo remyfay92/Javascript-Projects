a2a27112c18ab24689e918487c09612c5c0ae35c
remyfay92 <66988733+remyfay92@users.noreply.github.com>
2020-11-26 14:55:18 -0600

    fixed multiple js folder problem

--- Basic-Javascript-expressions/JS/alert(js)4.js
+++ Basic-Javascript-expressions/JS/alert(js)4.js
@@ -1,4 +1,14 @@
 window.alert("Here's A Alert");
 var one=("This is \"the\" fist half "); //variable assigned string
 var two=("this \"is\" the second half!"); //same as above
-document.write(one+two); //statement
+document.write(one+two) ; //statement
+
+var x="25";   //assigning variable
+var z="32";   //assigning variable
+document.write(x*z) ;   //statement
+
+var x="Emily", y="Amy", z="Anthony";  //multiple variables
+var x=x.fontcolor("purple");          // using statement on variable
+var y=y.fontcolor("blue");            // using statement on variable
+var z=z.fontcolor("red");             // using statement on variable
+document.write(x,y,z) ;                //statement with multiple variables
