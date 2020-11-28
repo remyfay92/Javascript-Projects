5ff500a0480d1e2cc5b3db87fcab4a25d907a3b1
remyfay92 <66988733+remyfay92@users.noreply.github.com>
2020-11-27 23:08:27 -0600

    math drill

--- Basic-Javascript-expressions/Project3_math_operators/Js/main.js
+++ Basic-Javascript-expressions/Project3_math_operators/Js/main.js
@@ -1,5 +1,5 @@
 function add_Function() {
-    var add= 4+82;
-    document.getElementById("Math").innerHTML="4+82=" + add;
+    var add= 305-82;
+    document.getElementById("Math").innerHTML="305-82=" + add;
     return add_Function;
 }
