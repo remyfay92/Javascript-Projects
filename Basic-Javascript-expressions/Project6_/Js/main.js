557114253dd6dbe7f691400aa5e1494f0a3069ef
remyfay92 <66988733+remyfay92@users.noreply.github.com>
2020-11-29 09:55:40 -0600

    bug fix failed

--- Basic-Javascript-expressions/Project6_/Js/main.js
+++ Basic-Javascript-expressions/Project6_/Js/main.js
@@ -1,13 +1,15 @@
 function Doc_Function() {
-var Doc =(typeof 3);
-document.getElementById("exa1").innerHTML=Doc;          // this displays type of example in id exa1
-return Doc_Function
+    document.write(typeof 4);
 }
-var doc2 = document.write("10+5");
-document.getElementById("exa2").innerHTML= doc2;         //this displays string and number in id exa2
-
-document.getElementById("exa3").innerHTML= document.write(5<20);                     // this displays    in id exa3
-
-document.getElementById("exa4").innerHTML= document.write(10>4);                   // this displays     in id exa4
-
-document.getElementById("exa5").innerHTML= document.write(6==(2+4));                         // this displays    in id exa5
+function add_Function() {
+    document.write();
+}
+function lesser_Function() {
+document.write(5<20);                     // this displays    in id exa3
+}
+function greater_Function() {
+document.write(10>4);                   // this displays     in id exa4
+}
+function equal_Function() {
+ document.write(6==(2+4));                         // this displays    in id exa5
+}
