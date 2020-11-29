62d5a98b0b92a830399ec2a7354c998227d3935e
remyfay92 <66988733+remyfay92@users.noreply.github.com>
2020-11-29 10:34:40 -0600

    coercion drill

---
+++ Basic-Javascript-expressions/Project_Coercion/Js/main.js
@@ -0,0 +1,17 @@
+document.write(typeof 7);                             //here is a typeof example
+
+document.write("10"+9 );                                // adding a number to a string
+
+document.write(5<20);
+                                                            //these two are my greeater than and lesser than operators
+document.write( 10>8 );
+                                                         
+document.write(8==(4+4) );
+                                                               // my double and triple equals operators
+document.write(10===(5+5));
+
+document.write((6<2)&&(7>3));                                    // AND
+
+document.write((4<2)||(8>10));                                     // OR
+
+document.write(!(10+2));                                          // NOT
