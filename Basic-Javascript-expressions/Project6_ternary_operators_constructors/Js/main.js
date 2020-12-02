8c9a04efa2b2194b9f9f7e965965436eaad3d90e
remyfay92 <66988733+remyfay92@users.noreply.github.com>
2020-12-02 13:57:56 -0600

    project6 error

--- Basic-Javascript-expressions/Project6_ternary_operators_constructors/Js/main.js
+++ Basic-Javascript-expressions/Project6_ternary_operators_constructors/Js/main.js
@@ -1,12 +1,27 @@
-function PC(Brand, Ram, Storage, Prossesor) {
-    this.PC_Brand= Brand;
-    this.PC_Ram= Ram;
-    this.PC_Storage= Storage;
-    this.PC_Prossesor= Prossesor;
+function my1Pc(z,x) {
+    document.write(x+"'s are great Computers so are "+z+"'s.<br>");
+}
+function my2Pc(z,x) {
+    document.write("My secound Computer was a " +z+"<br>");
+    my1Pc(z,x);
+}
+function displayFunction() {
+    document.getElementById("nest_Id").innerHtml=my1Pc + my2Pc;
+}
+my2Pc("Dell", "Samsung");
+
+
+class PC {
+    constructor(Brand, Ram, Storage, Prossesor) {
+        this.PC_Brand = Brand;
+        this.PC_Ram = Ram;
+        this.PC_Storage = Storage;
+        this.PC_Prossesor = Prossesor;
+    }
 }
 var Comp_1 = new PC("HP", "8GB Ram", "500GB SSD", "Intel Core i7");
 var Comp_2 = new PC("Dell", "4GB Ram", "225GB SSD", "Intel Core i5");
 var Comp_3 = new PC("Samsung", "8GB Ram", "1TB SSD", "Intel Core i9");
 function N_T_Function() {
-    document.getElementById("New_and_This").innerHTML= "New Computer has these Fetures" + Comp_3.PC_Brand + Comp_3.PC_Ram + Comp_3.PC_Storage + Comp_3.PC_Prossesor;
+    document.getElementById("New_and_This").innerHTML= "New Computer has these Fetures"+ Comp_3.PC_Ram + Comp_3.PC_Storage + Comp_3.PC_Prossesor;
 }
